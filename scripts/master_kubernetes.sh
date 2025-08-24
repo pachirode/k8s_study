4ad89458932719820760b02b92ee1d3f725dd4a7
Rodepachi <pachirode@gmail.com>
2025-08-24 09:42:15 +0800

    完成部署 Master

--- scripts/master_kubernetes.sh
+++ scripts/master_kubernetes.sh
@@ -392,7 +392,7 @@ function master::k8s::pki() {
 # --service-node-port-range： 指定 NodePort 的端口范围
 
 # 如果 kube-apiserver 机器没有运行 kube-proxy，则还需要添加 --enable-aggregator-routing=true 参数
-function master::k8s::create-kube-apiserver-config() {
+function master::k8s::create-kube-apiserver-service() {
   source environment.sh
   cd /opt/k8s/work
 
@@ -504,4 +504,369 @@ function master::k8s::kube-apiserver-info() {
   kubectl get all --all-namespaces
   kubectl get componentstatuses
   sudo netstat -lnpt|grep kube
+}
+
+function master::k8s::kube-controller-manager() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  sudo tee kube-controller-manager-csr.json <<EOF
+  {
+      "CN": "system:kube-controller-manager",
+      "key": {
+          "algo": "rsa",
+          "size": 2048
+      },
+      "hosts": [
+        "127.0.0.1",
+        "192.168.29.130",
+        "192.168.29.131",
+        "192.168.29.132"
+      ],
+      "names": [
+        {
+          "C": "CN",
+          "ST": "ShangHai",
+          "L": "ShangHai",
+          "O": "system:kube-controller-manager",
+          "OU": "superproj"
+        }
+      ]
+  }
+EOF
+  cfssl gencert -ca=/opt/k8s/work/ca.pem \
+    -ca-key=/opt/k8s/work/ca-key.pem \
+    -config=/opt/k8s/work/ca-config.json \
+    -profile=kubernetes kube-controller-manager-csr.json | cfssljson -bare kube-controller-manager
+  ls kube-controller-manager*pem
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      scp kube-controller-manager*.pem root@${node_ip}:/etc/kubernetes/cert/
+    done
+}
+
+# 创建分发 kubeconfig
+function master::k8s::create-kube-api-config() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  kubectl config set-cluster kubernetes \
+    --certificate-authority=/opt/k8s/work/ca.pem \
+    --embed-certs=true \
+    --server="https://##NODE_IP##:6443" \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config set-credentials system:kube-controller-manager \
+    --client-certificate=kube-controller-manager.pem \
+    --client-key=kube-controller-manager-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config set-context system:kube-controller-manager \
+    --cluster=kubernetes \
+    --user=system:kube-controller-manager \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+  kubectl config use-context system:kube-controller-manager --kubeconfig=kube-controller-manager.kubeconfig
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      sed -e "s/##NODE_IP##/${node_ip}/" kube-controller-manager.kubeconfig > kube-controller-manager-${node_ip}.kubeconfig
+      scp kube-controller-manager-${node_ip}.kubeconfig root@${node_ip}:/etc/kubernetes/kube-controller-manager.kubeconfig
+    done
+}
+
+# 创建 kube-controller-manager.service
+# --secure-port=10252、--bind-address=0.0.0.0: 在所有网络接口监听 10252 端口的 https /metrics 请求
+# --kubeconfig：指定 kubeconfig 文件路径，kube-controller-manager 使用它连接和验证 kube-apiserver
+# --authentication-kubeconfig 和 --authorization-kubeconfig：kube-controller-manager 使用它连接 apiserver，对 client 的请求进行认证和授权。kube-controller-manager 不再使用 --tls-ca-file 对请求 https metrics 的 Client 证书进行校验。如果没有配置这两个 kubeconfig 参数，则 client 连接 kube-controller-manager https 端口的请求会被拒绝(提示权限不足)
+# --cluster-signing-*-file：签名 TLS Bootstrap 创建的证书
+# --root-ca-file：放置到容器 ServiceAccount 中的 CA 证书，用来对 kube-apiserver 的证书进行校验
+# --service-account-private-key-file：签名 ServiceAccount 中 Token 的私钥文件，必须和 kube-apiserver 的 --service-account-key-file 指定的公钥文件配对使用
+# --service-cluster-ip-range：指定 Service Cluster IP 网段，必须和 kube-apiserver 中的同名参数一致
+# --leader-elect=true：集群运行模式，启用选举功能；被选为 leader 的节点负责处理工作，其它节点为阻塞状态
+# --controllers=*,bootstrapsigner,tokencleaner：启用的控制器列表，tokencleaner 用于自动清理过期的 Bootstrap token
+# --horizontal-pod-autoscaler-*：custom metrics 相关参数，支持 autoscaling/v2alpha1
+# --tls-cert-file、--tls-private-key-file：使用 https 输出 metrics 时使用的 Server 证书和秘钥
+# --use-service-account-credentials=true: kube-controller-manager 中各 controller 使用 serviceaccount 访问 kube-apiserver
+function master::k8s::create-kube-controller-manager-service() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  sudo tee kube-controller-manager.service.template <<EOF
+  [Unit]
+  Description=Kubernetes Controller Manager
+  Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+
+  [Service]
+  WorkingDirectory=${K8S_DIR}/kube-controller-manager
+  ExecStart=/opt/k8s/bin/kube-controller-manager \\
+    --profiling \\
+    --cluster-name=kubernetes \\
+    --controllers=*,bootstrapsigner,tokencleaner \\
+    --kube-api-qps=1000 \\
+    --kube-api-burst=2000 \\
+    --leader-elect \\
+    --use-service-account-credentials\\
+    --concurrent-service-syncs=2 \\
+    --bind-address=##NODE_IP## \\
+    --secure-port=10252 \\
+    --tls-cert-file=/etc/kubernetes/cert/kube-controller-manager.pem \\
+    --tls-private-key-file=/etc/kubernetes/cert/kube-controller-manager-key.pem \\
+    --authentication-kubeconfig=/etc/kubernetes/kube-controller-manager.kubeconfig \\
+    --client-ca-file=/etc/kubernetes/cert/ca.pem \\
+    --requestheader-allowed-names="aggregator" \\
+    --requestheader-client-ca-file=/etc/kubernetes/cert/ca.pem \\
+    --requestheader-extra-headers-prefix="X-Remote-Extra-" \\
+    --requestheader-group-headers=X-Remote-Group \\
+    --requestheader-username-headers=X-Remote-User \\
+    --authorization-kubeconfig=/etc/kubernetes/kube-controller-manager.kubeconfig \\
+    --cluster-signing-cert-file=/etc/kubernetes/cert/ca.pem \\
+    --cluster-signing-key-file=/etc/kubernetes/cert/ca-key.pem \\
+    --horizontal-pod-autoscaler-sync-period=10s \\
+    --concurrent-deployment-syncs=10 \\
+    --concurrent-gc-syncs=30 \\
+    --node-cidr-mask-size=24 \\
+    --service-cluster-ip-range=${SERVICE_CIDR} \\
+    --terminated-pod-gc-threshold=10000 \\
+    --root-ca-file=/etc/kubernetes/cert/ca.pem \\
+    --service-account-private-key-file=/etc/kubernetes/cert/ca-key.pem \\
+    --kubeconfig=/etc/kubernetes/kube-controller-manager.kubeconfig \\
+    --v=2
+  Restart=on-failure
+  RestartSec=5
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
+  for (( i=0; i < 3; i++ ))
+    do
+      sed -e "s/##NODE_NAME##/${NODE_NAMES[i]}/" -e "s/##NODE_IP##/${NODE_IPS[i]}/" kube-controller-manager.service.template > kube-controller-manager-${NODE_IPS[i]}.service
+    done
+  ls kube-controller-manager*.service
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      scp kube-controller-manager-${node_ip}.service root@${node_ip}:/etc/systemd/system/kube-controller-manager.service
+    done
+}
+
+function master::k8s::kube-controller-manager-start() {
+  source environment.sh
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      ssh root@${node_ip} "mkdir -p ${K8S_DIR}/kube-controller-manager"
+      ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kube-controller-manager && systemctl restart kube-controller-manager"
+    done
+}
+
+function master::k8s::kube-controller-manager-status() {
+  source environment.sh
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      ssh root@${node_ip} "systemctl status kube-controller-manager|grep Active"
+    done
+}
+
+function master::k8s::kube-controller-manager-log() {
+  journalctl -u kube-controller-manager
+}
+
+# 查看输出的 metrics
+function master::k8s::kube-controller-manager-metrics::info() {
+  source environment.sh
+  curl -s --cacert /opt/k8s/work/ca.pem --cert /opt/k8s/work/admin.pem --key /opt/k8s/work/admin-key.pem https://${NODE_IPS[0]}:10252/metrics |head
+}
+
+function master::k8s::kube-controller-manager-leader() {
+  kubectl -n kube-system get leases kube-controller-manager
+}
+
+function master::k8s::kube-scheduler() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  sudo tee kube-scheduler-csr.json <<EOF
+  {
+      "CN": "system:kube-scheduler",
+      "hosts": [
+        "127.0.0.1",
+        "192.168.29.130",
+        "192.168.29.131",
+        "192.168.29.132"
+      ],
+      "key": {
+          "algo": "rsa",
+          "size": 2048
+      },
+      "names": [
+        {
+          "C": "CN",
+          "ST": "ShangHai",
+          "L": "ShangHai",
+          "O": "system:kube-scheduler",
+          "OU": "superproj"
+        }
+      ]
+  }
+EOF
+  cfssl gencert -ca=/opt/k8s/work/ca.pem \
+    -ca-key=/opt/k8s/work/ca-key.pem \
+    -config=/opt/k8s/work/ca-config.json \
+    -profile=kubernetes kube-scheduler-csr.json | cfssljson -bare kube-scheduler
+  ls kube-scheduler*pem
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      scp kube-scheduler*.pem root@${node_ip}:/etc/kubernetes/cert/
+    done
+}
+
+function master::k8s::create-kube-scheduler-config () {
+  source environment.sh
+  cd /opt/k8s/work
+
+  kubectl config set-cluster kubernetes \
+    --certificate-authority=/opt/k8s/work/ca.pem \
+    --embed-certs=true \
+    --server="https://##NODE_IP##:6443" \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config set-credentials system:kube-scheduler \
+    --client-certificate=kube-scheduler.pem \
+    --client-key=kube-scheduler-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config set-context system:kube-scheduler \
+    --cluster=kubernetes \
+    --user=system:kube-scheduler \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+  kubectl config use-context system:kube-scheduler --kubeconfig=kube-scheduler.kubeconfig
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      sed -e "s/##NODE_IP##/${node_ip}/" kube-scheduler.kubeconfig > kube-scheduler-${node_ip}.kubeconfig
+      scp kube-scheduler-${node_ip}.kubeconfig root@${node_ip}:/etc/kubernetes/kube-scheduler.kubeconfig
+    done
+}
+
+function master::k8s::create-kube-scheduler-yaml() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  sudo tee kube-scheduler.yaml.template <<EOF
+  apiVersion: kubescheduler.config.k8s.io/v1
+  kind: KubeSchedulerConfiguration
+  clientConnection:
+    burst: 200
+    kubeconfig: "/etc/kubernetes/kube-scheduler.kubeconfig"
+    qps: 100
+  enableContentionProfiling: false
+  enableProfiling: true
+  leaderElection:
+    leaderElect: true
+EOF
+  for (( i=0; i < 3; i++ ))
+    do
+      sed -e "s/##NODE_NAME##/${NODE_NAMES[i]}/" -e "s/##NODE_IP##/${NODE_IPS[i]}/" kube-scheduler.yaml.template > kube-scheduler-${NODE_IPS[i]}.yaml
+    done
+  ls kube-scheduler*.yaml
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      scp kube-scheduler-${node_ip}.yaml root@${node_ip}:/etc/kubernetes/kube-scheduler.yaml
+    done
+}
+
+function master::k8s::create-kube-scheduler-service() {
+  source environment.sh
+  cd /opt/k8s/work
+
+  sudo tee kube-scheduler.service.template <<EOF
+  [Unit]
+  Description=Kubernetes Scheduler
+  Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+
+  [Service]
+  WorkingDirectory=${K8S_DIR}/kube-scheduler
+  ExecStart=/opt/k8s/bin/kube-scheduler \\
+    --config=/etc/kubernetes/kube-scheduler.yaml \\
+    --bind-address=##NODE_IP## \\
+    --secure-port=10259 \\
+    --tls-cert-file=/etc/kubernetes/cert/kube-scheduler.pem \\
+    --tls-private-key-file=/etc/kubernetes/cert/kube-scheduler-key.pem \\
+    --authentication-kubeconfig=/etc/kubernetes/kube-scheduler.kubeconfig \\
+    --client-ca-file=/etc/kubernetes/cert/ca.pem \\
+    --requestheader-allowed-names="" \\
+    --requestheader-client-ca-file=/etc/kubernetes/cert/ca.pem \\
+    --requestheader-extra-headers-prefix="X-Remote-Extra-" \\
+    --requestheader-group-headers=X-Remote-Group \\
+    --requestheader-username-headers=X-Remote-User \\
+    --authorization-kubeconfig=/etc/kubernetes/kube-scheduler.kubeconfig \\
+    --v=2
+  Restart=always
+  RestartSec=5
+  StartLimitInterval=0
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
+  for (( i=0; i < 3; i++ ))
+    do
+      sed -e "s/##NODE_NAME##/${NODE_NAMES[i]}/" -e "s/##NODE_IP##/${NODE_IPS[i]}/" kube-scheduler.service.template > kube-scheduler-${NODE_IPS[i]}.service
+    done
+  ls kube-scheduler*.service
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      scp kube-scheduler-${node_ip}.service root@${node_ip}:/etc/systemd/system/kube-scheduler.service
+    done
+}
+
+function master::k8s::kube-scheduler-start() {
+  source environment.sh
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      ssh root@${node_ip} "mkdir -p ${K8S_DIR}/kube-scheduler"
+      ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kube-scheduler && systemctl restart kube-scheduler"
+    done
+}
+
+function master::k8s::kube-scheduler-status() {
+  source environment.sh
+
+  for node_ip in ${NODE_IPS[@]}
+    do
+      echo ">>> ${node_ip}"
+      ssh root@${node_ip} "systemctl status kube-scheduler|grep Active"
+    done
+}
+
+function master::k8s::kube-scheduler-log() {
+  journalctl -u kube-scheduler
+}
+
+function master::k8s::kube-scheduler::metrics::info () {
+  source environment.sh
+  sudo netstat -lnpt |grep kube-sch
+
+  curl -s --cacert /opt/k8s/work/ca.pem --cert /opt/k8s/work/admin.pem --key /opt/k8s/work/admin-key.pem https://${NODE_IPS[0]}:10259/metrics |head
+}
+
+function master::k8s::kube-scheduler::leader() {
+  kubectl -n kube-system get leases kube-scheduler
 }
